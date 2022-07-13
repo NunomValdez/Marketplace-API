77616b365dc3dda6f56f6a87c0594a6766a30691
Nuno Valdez <nunomvmf@gmail.com>
2022-07-13 20:33:48 +0100

    swagger and orders initialized

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { OrderController } from './order/order.controller';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [AppController],
+  controllers: [AppController, OrderController],
   providers: [AppService],
 })
 export class AppModule {}
