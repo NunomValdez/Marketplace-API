d1aaa55c8e0f839a2c1154c9eccf57fd0785139d
Nuno Valdez <nunomvmf@gmail.com>
2022-07-13 20:35:34 +0100

    shipment and warehouse created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { OrderController } from './order/order.controller';
+import { ShipmentController } from './shipment/shipment.controller';
+import { WarehouseController } from './warehouse/warehouse.controller';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [AppController, OrderController],
+  controllers: [AppController, OrderController, ShipmentController, WarehouseController],
   providers: [AppService],
 })
 export class AppModule {}
